62c52d6ff2c7c85676a9a45ef74696067fb16619
rhylie-beevers <rhylie.beevers@gmail.com>
2025-11-07 16:17:38 -0700

    added config and part of get_files_content function

--- functions/get_file_content.py
+++ functions/get_file_content.py
@@ -1,4 +1,15 @@
 import os
+from config import *
 
 def get_file_content(working_directory, file_path):
-    pass
+    working_abs_path = os.path.abspath(working_directory)
+    full_path = os.path.join(working_directory, file_path)
+    abs_path = os.path.abspath(full_path)
+    if not working_abs_path.endswith(os.sep):
+        working_abs_path += os.sep
+    if not (abs_path.startswith(working_abs_path) or abs_path == os.path.abspath(working_directory)):
+        return f'Error: Cannot list "{file_path}" as it is outside the permitted working directory' 
+    if not os.path.isfile(abs_path):
+        return f'Error: File not found or is not a regular file: "{file_path}"'
+    with open(file_path, "r") as f:
+        pass
